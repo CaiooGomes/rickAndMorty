5234152dc347e3745958a44dfcdfe157f6603657
CaiooGomes <gomescaio898@gmail.com>
2023-05-18 16:33:21 -0300

    pegando os dados

---
+++ scripts/navbar.js
@@ -0,0 +1,6 @@
+const navbar = document.getElementById("navbar")
+navbar.innerHTML= `<div>
+    <a class="links-navbar" href="./index.html">Home</a>
+    <a class="links-navbar" href="./cartoons.html">Cartoons</a>
+    </div>
+    <img id="navbar-image" src="./images/NavBar/morty.jpeg">`
