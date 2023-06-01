80bae4b1ac17bdd647297eaf2f7911600700f710
CaiooGomes <gomescaio898@gmail.com>
2023-06-01 16:58:24 -0300

    sendMessage criado com javascript

--- scripts/zap.js
+++ scripts/zap.js
@@ -1,5 +1,6 @@
+zap = document.getElementById("zap")
+zap.innerHTML = '<img id="sendMessageBtn" src="https://i.pinimg.com/originals/79/dc/31/79dc31280371b8ffbe56ec656418e122.png" alt="">'
 document.getElementById("sendMessageBtn").addEventListener("click",()=>SendMessage());
-
 const SendMessage = ()=>{
     var phoneNumber = "12988999999";
     var message = "olá gostei muito do seu site";
